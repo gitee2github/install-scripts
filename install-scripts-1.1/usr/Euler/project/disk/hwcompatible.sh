50082b02cda6ac816a9217cfee0e5709952bde03
zhangqiumiao <zhangqiumiao1@huawei.com>
2022-03-01 11:44:48 +0800

    support x86 UEFI without /boot/efi partition and add license statement

--- install-scripts-1.1/usr/Euler/project/disk/hwcompatible.sh
+++ install-scripts-1.1/usr/Euler/project/disk/hwcompatible.sh
@@ -1,5 +1,19 @@
+:<<!
+* Copyright (c) Huawei Technologies Co., Ltd. 2013-2022. All rights reserved.
+* install-scripts licensed under the Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*     http://license.coscl.org.cn/MulanPSL2
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+* PURPOSE.
+* See the Mulan PSL v2 for more details.
+* Author: zhangqiumiao
+* Create: 2022-02-28
+* Description: functions for compatibility
+!
+
 #!/bin/bash
-# Description: functions for compatibility
 
 #machine type
 CON_MACHINE_TYPE_GENERAL="general"
